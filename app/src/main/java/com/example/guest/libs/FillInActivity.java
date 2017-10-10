a453a18067471e3649554cfdf525c99d96f736ed
Kate Byars <katebyars5@gmail.com>
2017-10-10 13:21:53 -0700

    finalize the project.

--- app/src/main/java/com/example/guest/libs/FillInActivity.java
+++ app/src/main/java/com/example/guest/libs/FillInActivity.java
@@ -1,5 +1,4 @@
 package com.example.guest.libs;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -12,7 +11,6 @@ import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class FillInActivity extends AppCompatActivity {
-    private Button mCreateStoryButton;
     @Bind(R.id.noun1) TextView mNoun1;
     @Bind(R.id.location2) TextView mLocation2;
     @Bind(R.id.object1) TextView mObject1;
@@ -25,7 +23,7 @@ public class FillInActivity extends AppCompatActivity {
     @Bind(R.id.occupation) TextView mOccupation;
     @Bind(R.id.food1) TextView mFood1;
     @Bind(R.id.food2) TextView mFood2;
-    @Bind(R.id.createStoryButton) Button getmCreateStoryButton;
+    @Bind(R.id.createStoryButton) Button mCreateStoryButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,17 +36,16 @@ public class FillInActivity extends AppCompatActivity {
             public void onClick(View view) {
                 String noun1 = mNoun1.getText().toString();
                 String location2 = mLocation2.getText().toString();
-                String object1 = mNoun1.getText().toString();
-                String object2 = mNoun1.getText().toString();
-                String adjective1 = mNoun1.getText().toString();
-                String adjective2 = mNoun1.getText().toString();
-                String actionword1 = mNoun1.getText().toString();
-                String largeflatobject = mNoun1.getText().toString();
-                String kindofdwelling = mNoun1.getText().toString();
-                String bodypart = mNoun1.getText().toString();
-                String occupation = mNoun1.getText().toString();
-                String food1 = mNoun1.getText().toString();
-                String food2 = mNoun1.getText().toString();
+                String object1 = mObject1.getText().toString();
+                String object2 = mObject2.getText().toString();
+                String adjective1 = mAdjective1.getText().toString();
+                String actionword1 = mActionword1.getText().toString();
+                String largeflatobject = mLargeflatobject.getText().toString();
+                String kindofdwelling = mKindofdwelling.getText().toString();
+                String bodypart = mBodypart.getText().toString();
+                String occupation = mOccupation.getText().toString();
+                String food1 = mFood1.getText().toString();
+                String food2 = mFood2.getText().toString();
 
                 Intent intent = new Intent(FillInActivity.this, StoryActivity.class);
                 intent.putExtra("noun1", noun1);
